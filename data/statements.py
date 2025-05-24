76948d11906391d886572b29d5490370c0275735
xkazm04 <kazdan@seznam.cz>
2025-05-24 15:31:08 +0200

    Prompts and statements

---
+++ data/statements.py
@@ -0,0 +1,11 @@
+
+# Test data
+## List of statemets
+
+### 1. Historical statements - No web research needed, LLM knowledge sufficient to response. Goal: Verify thinking process and output structure of agents
+
+### 2. Present statements - Web research needed
+st_biden_cancer = """
+Former President Joe Biden has been diagnosed with prostate cancer. The type of cancer he has is considered "aggressive," but is it also "turbo," as some on social media have said? 
+"It is sad to see another one who is Covid vaccinated and boosted it to the hilt, get turbo cancer already metastasized,"
+"""
